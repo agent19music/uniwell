b9a0edbc2c25f1cdce03a2cfa90957cec8205038
Sean <seanmotanya@gmail.com>
2025-04-30 11:58:20 +0300

    Update timetable types

--- types/TimetableTypes.ts
+++ types/TimetableTypes.ts
@@ -45,15 +45,53 @@ export interface ClassSchedule {
     instructor: string;
     frequency: ClassFrequency;
     type: ClassType;
-    startTime: Date;
-    endTime: Date;
-    classType: string;
+    startTime: string; // Format: "HH:mm:ss"
+    endTime: string; // Format: "HH:mm:ss"
+    daysOfWeek: string[]; // Array of day names
+    notificationPreference: {
+        beforeClass: number;
+        afterClass: number | null;
+        onMorning: number;
+    }   
+}
+export interface ServerResponseClassSChedule{
+    id:string;
+    user_id:string;
+    semester_id:string;
+    course_name:string;
+    room: string;
+    course_code:string;
+    instructor: string;
+    frequency: ClassFrequency;
+    type: ClassType;
+    start_time: string; // Format: "HH:mm:ss"
+    end_time: string; // Format: "HH:mm:ss"
+    days_of_week: string[]; // Array of day names
+    notification_preference: {
+        beforeClass: number;
+        afterClass: number | null;
+        onMorning: number;
+    }   
+
+    
+}
+
+
+export interface NewClassSchedule {
+    courseName: string;
+    room: string;
+    courseCode: string;
+    instructor: string;
+    frequency: ClassFrequency;
+    type: ClassType;
+    startTime: string; // Format: "HH:mm"
+    endTime: string; // Format: "HH:mm"
     daysOfWeek: DayOfTheWeek[];
     notificationPreference: {
         beforeClass: number;
         afterClass: number | null;
-       onMorning: number;
-    }   
+        onMorning: number;
+    }
 }
 
 export interface AttendanceRecord {
